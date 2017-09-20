f30254c7155d624cab309f9f9747a0df60321640
Artemas <artemasseng@gmail.com>
2017-09-20 23:24:20 +0100

    First Commit

---
+++ src/main/java/com/activiti/demo/ActivitiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.activiti.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiDemoApplication.class, args);
+	}
+}
