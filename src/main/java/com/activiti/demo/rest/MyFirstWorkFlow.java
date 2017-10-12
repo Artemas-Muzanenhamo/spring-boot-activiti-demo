ebf91ee7a1cf79a4d740d1547d95e87e53657a42
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-10-12 12:25:03 +0100

    Added @CrossOrigin for angular requests

--- src/main/java/com/activiti/demo/rest/MyFirstWorkFlow.java
+++ src/main/java/com/activiti/demo/rest/MyFirstWorkFlow.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.activiti.demo.model.TaskObject;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/process")
 public class MyFirstWorkFlow {
