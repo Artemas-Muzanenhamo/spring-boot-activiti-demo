8715d6cc9590cab46e3220dd30694c39b781adca
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-12-28 19:06:31 +0000

    Add TaskAssignee converter

---
+++ src/main/java/com/activiti/demo/json/TaskAssignee.java
@@ -0,0 +1,15 @@
+package com.activiti.demo.json;
+
+public class TaskAssignee {
+    private String taskAssignee;
+
+    public TaskAssignee() { }
+
+    public TaskAssignee(String taskAssignee) {
+        this.taskAssignee = taskAssignee;
+    }
+
+    public String getTaskAssignee() {
+        return taskAssignee;
+    }
+}
---
+++ src/test/java/com/activiti/demo/converter/TaskAssigneeConverterTest.java
@@ -0,0 +1,25 @@
+package com.activiti.demo.converter;
+
+import com.activiti.demo.json.TaskAssignee;
+import com.activiti.demo.json.TaskAssigneeJson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.activiti.demo.converter.TaskAssigneeConverter.taskAssigneeJsonToDto;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TaskAssigneeConverterTest {
+
+    private static final String ASSIGNEE = "some-assignee";
+
+    @Test
+    @DisplayName("Should convert TaskAssigneeJson to DTO")
+    void convertTaskAssigneeJsonToDto() {
+        TaskAssigneeJson taskAssigneeJson = new TaskAssigneeJson(ASSIGNEE);
+
+        TaskAssignee taskAssignee = taskAssigneeJsonToDto(taskAssigneeJson);
+
+        assertThat(taskAssignee).isNotNull();
+        assertThat(taskAssignee.getTaskAssignee()).isEqualTo(ASSIGNEE);
+    }
+}
