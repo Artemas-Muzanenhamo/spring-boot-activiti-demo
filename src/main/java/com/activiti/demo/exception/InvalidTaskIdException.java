a1670e5bfea3ceb9550989808c2979e07ad77d14
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-12-31 00:37:13 +0000

    polish up

--- src/main/java/com/activiti/demo/exception/InvalidTaskIdException.java
+++ src/main/java/com/activiti/demo/exception/InvalidTaskIdException.java
@@ -1,9 +1,6 @@
 package com.activiti.demo.exception;
 
 public class InvalidTaskIdException extends RuntimeException {
-    public InvalidTaskIdException() {
-    }
-
     public InvalidTaskIdException(String message) {
         super(message);
     }
